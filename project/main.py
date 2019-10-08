60c61f0d80c61f28082a3efbe9231623d36b65ca
shahriya1995 <shahriya1995@gmail.com>
2019-10-08 11:04:37 -0700

    Updated html pages

--- project/main.py
+++ project/main.py
@@ -13,3 +13,8 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/checkout')
+@login_required
+def checkout():
+    return render_template('checkout.html', name=current_user.name)
